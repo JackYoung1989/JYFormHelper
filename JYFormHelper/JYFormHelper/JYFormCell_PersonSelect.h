e3161680532e98a6de7541a054459d2da78dbbec
JackYoung <yangjieseagull@163.com>
2021-02-06 10:24:49 +0800

    创建选择人员的cell

---
+++ JYFormHelper/JYFormHelper/JYFormCell_PersonSelect.h
@@ -0,0 +1,16 @@
+//
+//  JYFormCell_PersonSelect.h
+//  JYFormHelper
+//
+//  Created by JackYoung on 2021/2/6.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JYFormCell_PersonSelect : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
